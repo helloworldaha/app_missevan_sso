c0dc2a4dee504116a461f196d925d51ceac12d7c
tengattack <tengattack@foxmail.com>
2015-01-24 17:53:39 +0800

    add model base

---
+++ model/account.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+* model/account.js
+* MissEvan SSO
+*
+* Author: 腾袭
+*
+* account model
+*/
+
+var util = require('util');
+
+var mysql = require('mysql');
+
+function Account() {
+
+}
+
+
+module.exports = Account;
---
+++ model/session.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+* model/session.js
+* MissEvan SSO
+*
+* Author: 腾袭
+*
+* session model
+*/
+
+var util = require('util');
+
+var ObjectID = require('mongodb').ObjectID;
+
+function Session() {
+
+}
+
+
+module.exports = Seesion;
